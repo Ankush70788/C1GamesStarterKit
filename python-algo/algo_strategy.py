ce0bc14ba96be3c656711573224d02bed961e225
Ankush Chatterjee <115393830+Ankush70788@users.noreply.github.com>
2024-06-25 14:35:48 +0530

    Update algo_strategy.py

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -45,6 +45,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         self.scored_on_locations = []
 
     def on_turn(self, turn_state):
+
         """
         This function is called every turn with the game state wrapper as
         an argument. The wrapper stores the state of the arena and has methods
@@ -53,6 +54,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         game engine.
         """
         game_state = gamelib.GameState(self.config, turn_state)
+        game_state.attempt_spawn(DEMOLISHER, [24, 10], 3)
         gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
         game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
 
